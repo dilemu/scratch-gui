a2682ffaf735d108c3e4dc6495d799ae02d6d7b9
pan9 <zrking_sun@163.com>
2021-07-28 00:59:29 +0800

    主页标题修改

--- webpack.config.js
+++ webpack.config.js
@@ -151,7 +151,7 @@ module.exports = [
             new HtmlWebpackPlugin({
                 chunks: ['lib.min', 'gui'],
                 template: 'src/playground/index.ejs',
-                title: 'OpenBlock GUI',
+                title: '迪乐姆儿童编程',
                 sentryConfig: process.env.SENTRY_CONFIG ? '"' + process.env.SENTRY_CONFIG + '"' : null
             }),
             new HtmlWebpackPlugin({
