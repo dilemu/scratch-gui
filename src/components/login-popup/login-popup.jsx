a5b64086820680ce09b77a78088d367f59a64732
pan9 <zrking_sun@163.com>
2021-08-27 00:30:46 +0800

    feat: login popup logic

--- src/components/login-popup/login-popup.jsx
+++ src/components/login-popup/login-popup.jsx
@@ -1,72 +1,106 @@
-import Modal from '../../containers/modal.jsx';
-import styles from './login-popup.css';
-import React from 'react';
-import PropTypes from 'prop-types';
-import Box from '../box/box.jsx';
-import connect from 'react-redux/es/connect/connect';
-import classNames from 'classnames';
-import {FormattedMessage} from 'react-intl';
-import Button from '../button/button.jsx';
-import {closeLoginModal} from '../../reducers/modals';
+import Modal from "../../containers/modal.jsx";
+import styles from "./login-popup.css";
+import React from "react";
+import PropTypes from "prop-types";
+import Box from "../box/box.jsx";
+import connect from "react-redux/es/connect/connect";
+import classNames from "classnames";
+import { FormattedMessage } from "react-intl";
+import Button from "../button/button.jsx";
+import { closeLoginModal } from "../../reducers/modals";
+import request from "../../public/request"
+import {loginSuccess, loginError} from "../../reducers/user-state"
 
 class LoginPopup extends React.Component {
+    loginServer(account, password) {
+      const url = '/api/user/login'
+      const data = {
+        userName: account,
+        password: password
+      }
+      const _this = this
+      request({url,data,method:"POST"}).then(res => {
+        if(res.code == 200) {
+          console.log('login success', res.data)
+          _this.props.loginSuccess(res.data)
+          _this.props.onCancel()
+        } else {
+          console.log('login failure', res.message)
+          _this.props.loginError(res.message)
+          alert("账号或密码错误！")
+        }
+      }).catch(err => ({ err }))
+    }
 
-    render () {
-      return (
-        <Modal
-          className = {styles.modalContent}
-          conyentLabel={this.props.title}
-          id="login-popup"
-          onRequestClose={this.props.onCancel}
-        >
-          <Box>
-            <input
-              className={styles.minInput}
-              name="account"
-              placeholder="账号"
-              type="text"
-            /><br />
-            
-            <input
-              className={styles.minInput}
-              name="password"
-              placeholder="密码"
-              type="password"
-            /><br />
+    getAccountAndPasswod() {
+      const {account, password} = this.refs;
+      if(account.value !== '' & password.value !== '') {
+        this.loginServer(account.value, password.value)
+      } else {
+        alert('账号或密码不能为空！')
+      }
+    };
 
-            <Button
-              className={classNames(
-                styles.btnSubmit,
-                styles.submitLoginButton
-              )}
-              onClick={this.props.submitLogin}
+    render() {
+        return (
+            <Modal
+                className={styles.modalContent}
+                conyentLabel={this.props.title}
+                id="login-popup"
+                onRequestClose={this.props.onCancel}
             >
-              <FormattedMessage
-                defaultMessage="登录"
-                description="Label for submit login"
-                id="gui.menuBar.loginButton"
-              />
-            </Button>
-          </Box>
-        </Modal>
-      )
+                <Box>
+                    <input
+                        ref="account"
+                        className={styles.minInput}
+                        name="account"
+                        placeholder="账号"
+                        type="text"
+                    />
+                    <br />
+
+                    <input
+                        ref="password"
+                        className={styles.minInput}
+                        name="password"
+                        placeholder="密码"
+                        type="password"
+                    />
+                    <br />
+
+                    <Button
+                        className={classNames(
+                            styles.btnSubmit,
+                            styles.submitLoginButton
+                        )}
+                        // onClick={this.props.submitLogin}
+                        onClick={this.getAccountAndPasswod.bind(this)}
+                    >
+                        <FormattedMessage
+                            defaultMessage="登录"
+                            description="Label for submit login"
+                            id="gui.menuBar.loginButton"
+                        />
+                    </Button>
+                </Box>
+            </Modal>
+        );
     }
 }
 
 LoginPopup.propTypes = {
     title: PropTypes.string.isRequired,
     onCancel: PropTypes.func.isRequired,
-    submitLogin: PropTypes.func
-}
+    submitLogin: PropTypes.func,
+};
 
-const mapStateToProps = state => ({state:state})
+const mapStateToProps = (state) => ({ state: state });
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
     onCancel: () => dispatch(closeLoginModal()),
-    submitLogin: () => {}
+    loginSuccess: data => dispatch(loginSuccess(data)),
+    loginError: err => dispatch(loginError(err)),
+    submitLogin: () => {},
 });
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(LoginPopup);
+export default connect(mapStateToProps, mapDispatchToProps)(LoginPopup);
---
+++ src/public/request.js
@@ -0,0 +1,36 @@
+/**
+ * 封装fetch请求
+ */
+ 
+// 全局路径
+const commonUrl = 'http://localhost:60002'
+export const parseJSON = function (response) {
+    return response.json();
+};
+export const checkStatus = function (response) {
+    if (response.status >= 200 && response.status < 500){
+        return response;
+    }
+    const error = new Error(response.statusText)
+    error.response = response
+    throw error;
+};
+// eslint-disable-next-line require-jsdoc
+export default function request (options = {}) {
+    const {data, url} = options
+    options = {...options}
+    // 跨域
+    options.mode = 'cors'
+    delete options.url
+    if (data) {
+        delete options.data
+        options.body = JSON.stringify(data);
+    }
+    options.headers = {
+        'Content-Type': 'application/json'
+    }
+    return fetch(commonUrl + url, options, {credentials: 'include'})
+        .then(checkStatus)
+        .then(parseJSON)
+        .catch(err => ({err}));
+}
