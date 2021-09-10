84fbc76cdeb933f91907e7b4c7ed2e8f02099abf
pan9 <zrking_sun@163.com>
2021-09-10 22:52:16 +0800

    feat: add upload modal

--- src/components/upload-modal/upload-modal.jsx
+++ src/components/upload-modal/upload-modal.jsx
@@ -1,47 +1,63 @@
-import React, { useState } from "react";
-import { Modal, Button, Upload, UploadOutlined } from "antd";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
+import { Modal, Button, Upload } from "antd";
+import { UploadOutlined } from '@ant-design/icons';
 
 const UploadModal = props => {
-    const [isModalVisible, setIsModalVisible] = useState(true);
+    const { className, vm } = props;
+    const [uuid, setUuid] = useState(Math.random());
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
     const showModal = () => {
         setIsModalVisible(true);
     };
 
-    const handleOk = () => {
-        setIsModalVisible(false);
-    };
-
     const handleCancel = () => {
+        vm.runtime.emit(uuid, null);
         setIsModalVisible(false);
     };
 
-    const props = {
+    const start = (uuid) => {
+        showModal();
+        setUuid(uuid);
+        console.log('上传窗口：'+uuid);
+    };
+
+    useEffect(() => {
+        console.log("上传窗口初始化");
+        vm.runtime.on("start_upload_file", start);
+    }, []);
+
+    const uploadProps = {
         name: "file",
-        onChange(info) {
-            if (info.file.status !== "uploading") {
-                console.log(info.file, info.fileList);
-            }
-            if (info.file.status === "done") {
-                message.success(`${info.file.name} file uploaded successfully`);
-            } else if (info.file.status === "error") {
-                message.error(`${info.file.name} file upload failed.`);
-            }
+        beforeUpload: file => {
+            vm.runtime.emit(uuid, file);
+            handleCancel()
+            return false
         },
+        showUploadList: false
     };
 
     return (
         <Modal
             title="上传"
             visible={isModalVisible}
-            onOk={handleOk}
             onCancel={handleCancel}
+            footer={[
+                <Button key="back" onClick={handleCancel}>返回</Button>
+            ]}
         >
-            <Upload {...props}>
-                <Button icon={<UploadOutlined />}>Click to Upload</Button>
+            <Upload {...uploadProps}>
+                <Button type="primary" icon={<UploadOutlined />}>点击上传</Button>
             </Upload>
         </Modal>
     );
 };
+
+UploadModal.propTypes = {
+    className: PropTypes.string,
+};
+
 
 export default UploadModal;
