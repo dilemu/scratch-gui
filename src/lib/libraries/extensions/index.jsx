0414cd526deb89f3367726f8d58320e9d484f66a
pan9 <zrking_sun@163.com>
2021-11-16 22:43:44 +0800

    v0.1.40

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -266,131 +266,131 @@ export default [
     //     featured: true,
     //     internetConnectionRequired: true,
     // },
-    // {
-    //     name: (
-    //         <FormattedMessage
-    //             defaultMessage="Music"
-    //             description="Name for the 'Music' extension"
-    //             id="gui.extension.music.name"
-    //         />
-    //     ),
-    //     extensionId: "music",
-    //     iconURL: musicIconURL,
-    //     insetIconURL: musicInsetIconURL,
-    //     description: (
-    //         <FormattedMessage
-    //             defaultMessage="Play instruments and drums."
-    //             description="Description for the 'Music' extension"
-    //             id="gui.extension.music.description"
-    //         />
-    //     ),
-    //     featured: true,
-    // },
-    // {
-    //     name: (
-    //         <FormattedMessage
-    //             defaultMessage="Pen"
-    //             description="Name for the 'Pen' extension"
-    //             id="gui.extension.pen.name"
-    //         />
-    //     ),
-    //     extensionId: "pen",
-    //     iconURL: penIconURL,
-    //     insetIconURL: penInsetIconURL,
-    //     description: (
-    //         <FormattedMessage
-    //             defaultMessage="Draw with your sprites."
-    //             description="Description for the 'Pen' extension"
-    //             id="gui.extension.pen.description"
-    //         />
-    //     ),
-    //     featured: true,
-    // },
-    // {
-    //     name: (
-    //         <FormattedMessage
-    //             defaultMessage="Video Sensing"
-    //             description="Name for the 'Video Sensing' extension"
-    //             id="gui.extension.videosensing.name"
-    //         />
-    //     ),
-    //     extensionId: "videoSensing",
-    //     iconURL: videoSensingIconURL,
-    //     insetIconURL: videoSensingInsetIconURL,
-    //     description: (
-    //         <FormattedMessage
-    //             defaultMessage="Sense motion with the camera."
-    //             description="Description for the 'Video Sensing' extension"
-    //             id="gui.extension.videosensing.description"
-    //         />
-    //     ),
-    //     featured: true,
-    // },
-    // {
-    //     name: (
-    //         <FormattedMessage
-    //             defaultMessage="Text to Speech"
-    //             description="Name for the Text to Speech extension"
-    //             id="gui.extension.text2speech.name"
-    //         />
-    //     ),
-    //     extensionId: "text2speech",
-    //     collaborator: "Amazon Web Services",
-    //     iconURL: text2speechIconURL,
-    //     insetIconURL: text2speechInsetIconURL,
-    //     description: (
-    //         <FormattedMessage
-    //             defaultMessage="Make your projects talk."
-    //             description="Description for the Text to speech extension"
-    //             id="gui.extension.text2speech.description"
-    //         />
-    //     ),
-    //     featured: true,
-    //     internetConnectionRequired: true,
-    // },
-    // {
-    //     name: (
-    //         <FormattedMessage
-    //             defaultMessage="Translate"
-    //             description="Name for the Translate extension"
-    //             id="gui.extension.translate.name"
-    //         />
-    //     ),
-    //     extensionId: "translate",
-    //     collaborator: "Google",
-    //     iconURL: translateIconURL,
-    //     insetIconURL: translateInsetIconURL,
-    //     description: (
-    //         <FormattedMessage
-    //             defaultMessage="Translate text into many languages."
-    //             description="Description for the Translate extension"
-    //             id="gui.extension.translate.description"
-    //         />
-    //     ),
-    //     featured: true,
-    //     internetConnectionRequired: true,
-    // },
-    // {
-    //     name: (
-    //         <FormattedMessage
-    //             defaultMessage="Makey Makey"
-    //             description="Name for the Makey Makey extension"
-    //             id="gui.extension.makeymakey.name"
-    //         />
-    //     ),
-    //     extensionId: "makeymakey",
-    //     collaborator: "JoyLabz",
-    //     iconURL: makeymakeyIconURL,
-    //     insetIconURL: makeymakeyInsetIconURL,
-    //     description: (
-    //         <FormattedMessage
-    //             defaultMessage="Make anything into a key."
-    //             description="Description for the 'Makey Makey' extension"
-    //             id="gui.extension.makeymakey.description"
-    //         />
-    //     ),
-    //     featured: true,
-    // },
+    {
+        name: (
+            <FormattedMessage
+                defaultMessage="Music"
+                description="Name for the 'Music' extension"
+                id="gui.extension.music.name"
+            />
+        ),
+        extensionId: "music",
+        iconURL: musicIconURL,
+        insetIconURL: musicInsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Play instruments and drums."
+                description="Description for the 'Music' extension"
+                id="gui.extension.music.description"
+            />
+        ),
+        featured: true,
+    },
+    {
+        name: (
+            <FormattedMessage
+                defaultMessage="Pen"
+                description="Name for the 'Pen' extension"
+                id="gui.extension.pen.name"
+            />
+        ),
+        extensionId: "pen",
+        iconURL: penIconURL,
+        insetIconURL: penInsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Draw with your sprites."
+                description="Description for the 'Pen' extension"
+                id="gui.extension.pen.description"
+            />
+        ),
+        featured: true,
+    },
+    {
+        name: (
+            <FormattedMessage
+                defaultMessage="Video Sensing"
+                description="Name for the 'Video Sensing' extension"
+                id="gui.extension.videosensing.name"
+            />
+        ),
+        extensionId: "videoSensing",
+        iconURL: videoSensingIconURL,
+        insetIconURL: videoSensingInsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Sense motion with the camera."
+                description="Description for the 'Video Sensing' extension"
+                id="gui.extension.videosensing.description"
+            />
+        ),
+        featured: true,
+    },
+    {
+        name: (
+            <FormattedMessage
+                defaultMessage="Text to Speech"
+                description="Name for the Text to Speech extension"
+                id="gui.extension.text2speech.name"
+            />
+        ),
+        extensionId: "text2speech",
+        collaborator: "Amazon Web Services",
+        iconURL: text2speechIconURL,
+        insetIconURL: text2speechInsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Make your projects talk."
+                description="Description for the Text to speech extension"
+                id="gui.extension.text2speech.description"
+            />
+        ),
+        featured: true,
+        internetConnectionRequired: true,
+    },
+    {
+        name: (
+            <FormattedMessage
+                defaultMessage="Translate"
+                description="Name for the Translate extension"
+                id="gui.extension.translate.name"
+            />
+        ),
+        extensionId: "translate",
+        collaborator: "Google",
+        iconURL: translateIconURL,
+        insetIconURL: translateInsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Translate text into many languages."
+                description="Description for the Translate extension"
+                id="gui.extension.translate.description"
+            />
+        ),
+        featured: true,
+        internetConnectionRequired: true,
+    },
+    {
+        name: (
+            <FormattedMessage
+                defaultMessage="Makey Makey"
+                description="Name for the Makey Makey extension"
+                id="gui.extension.makeymakey.name"
+            />
+        ),
+        extensionId: "makeymakey",
+        collaborator: "JoyLabz",
+        iconURL: makeymakeyIconURL,
+        insetIconURL: makeymakeyInsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Make anything into a key."
+                description="Description for the 'Makey Makey' extension"
+                id="gui.extension.makeymakey.description"
+            />
+        ),
+        featured: true,
+    },
     // {
     //     name: (
     //         <FormattedMessage
@@ -413,9 +413,9 @@ export default [
     //     featured: true,
     // },
     // {
-    //     name: 'LEGO MINDSTORMS EV3',
-    //     extensionId: 'ev3',
-    //     collaborator: 'LEGO',
+    //     name: "LEGO MINDSTORMS EV3",
+    //     extensionId: "ev3",
+    //     collaborator: "LEGO",
     //     iconURL: ev3IconURL,
     //     insetIconURL: ev3InsetIconURL,
     //     description: (
@@ -440,12 +440,12 @@ export default [
     //             id="gui.extension.ev3.connectingMessage"
     //         />
     //     ),
-    //     helpLink: 'https://scratch.mit.edu/ev3'
+    //     helpLink: "https://scratch.mit.edu/ev3",
     // },
     // {
-    //     name: 'LEGO BOOST',
-    //     extensionId: 'boost',
-    //     collaborator: 'LEGO',
+    //     name: "LEGO BOOST",
+    //     extensionId: "boost",
+    //     collaborator: "LEGO",
     //     iconURL: boostIconURL,
     //     insetIconURL: boostInsetIconURL,
     //     description: (
@@ -471,12 +471,12 @@ export default [
     //             id="gui.extension.boost.connectingMessage"
     //         />
     //     ),
-    //     helpLink: 'https://scratch.mit.edu/boost'
+    //     helpLink: "https://scratch.mit.edu/boost",
     // },
     // {
-    //     name: 'LEGO Education WeDo 2.0',
-    //     extensionId: 'wedo2',
-    //     collaborator: 'LEGO',
+    //     name: "LEGO Education WeDo 2.0",
+    //     extensionId: "wedo2",
+    //     collaborator: "LEGO",
     //     iconURL: wedo2IconURL,
     //     insetIconURL: wedo2InsetIconURL,
     //     description: (
@@ -502,12 +502,12 @@ export default [
     //             id="gui.extension.wedo2.connectingMessage"
     //         />
     //     ),
-    //     helpLink: 'https://scratch.mit.edu/wedo'
+    //     helpLink: "https://scratch.mit.edu/wedo",
     // },
     // {
-    //     name: 'Go Direct Force & Acceleration',
-    //     extensionId: 'gdxfor',
-    //     collaborator: 'Vernier',
+    //     name: "Go Direct Force & Acceleration",
+    //     extensionId: "gdxfor",
+    //     collaborator: "Vernier",
     //     iconURL: gdxforIconURL,
     //     insetIconURL: gdxforInsetIconURL,
     //     description: (
@@ -532,6 +532,6 @@ export default [
     //             id="gui.extension.gdxfor.connectingMessage"
     //         />
     //     ),
-    //     helpLink: 'https://scratch.mit.edu/vernier'
-    // }
+    //     helpLink: "https://scratch.mit.edu/vernier",
+    // },
 ];
